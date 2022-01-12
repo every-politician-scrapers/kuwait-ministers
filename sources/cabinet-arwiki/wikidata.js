b39592b95ce824152e63e3bb7083dbff31295370
Tony Bowden <tony@tmtm.com>
2022-01-12 09:25:02 +0200

    compare arwiki current cabinet members

---
+++ sources/cabinet-arwiki/wikidata.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+let rawmeta = fs.readFileSync('meta.json');
+let meta = JSON.parse(rawmeta);
+
+module.exports = function () {
+  return `SELECT DISTINCT (STRAFTER(STR(?item), STR(wd:)) AS ?wdid)
+               ?name ?wdLabel ?source ?sourceDate
+               (STRAFTER(STR(?positionItem), STR(wd:)) AS ?pid) ?position
+               ?startDate ?endDate
+               (STRAFTER(STR(?held), '/statement/') AS ?psid)
+        WHERE {
+          # Positions currently in the cabinet
+          ?positionItem p:P361 ?ps .
+          ?ps ps:P361 wd:${meta.cabinet.parent} .
+          FILTER NOT EXISTS { ?ps pq:P582 [] }
+
+          # Who currently holds those positions
+          ?item wdt:P31 wd:Q5 ; p:P39 ?held .
+          ?held ps:P39 ?positionItem ; pq:P580 ?startDate .
+          FILTER NOT EXISTS { ?held wikibase:rank wikibase:DeprecatedRank }
+          OPTIONAL { ?held pq:P582 ?endDate }
+
+          # Only get people in place at start of cabinet (source is not updated)
+          FILTER (?startDate <= "2018-01-22T00:00:00Z"^^xsd:dateTime)
+          FILTER (!BOUND(?endDate) || ?endDate >= "2018-01-23T00:00:00Z"^^xsd:dateTime)
+
+          OPTIONAL {
+            ?held prov:wasDerivedFrom ?ref .
+            ?ref pr:P4656 ?source FILTER CONTAINS(STR(?source), 'en.wikipedia.org') .
+
+            OPTIONAL { ?ref pr:P1810 ?sourceName }
+            OPTIONAL { ?ref pr:P1932 ?statedName }
+            OPTIONAL { ?ref pr:P813  ?sourceDate }
+          }
+
+          OPTIONAL { ?item rdfs:label ?wdLabel FILTER(LANG(?wdLabel) = "en") }
+          BIND(COALESCE(?sourceName, ?wdLabel) AS ?name)
+
+          OPTIONAL { ?positionItem wdt:P1705  ?nativeLabel   FILTER(LANG(?nativeLabel)   = "en") }
+          OPTIONAL { ?positionItem rdfs:label ?positionLabel FILTER(LANG(?positionLabel) = "en") }
+          BIND(COALESCE(?statedName, ?nativeLabel, ?positionLabel) AS ?position)
+        }
+        # ${new Date().toISOString()}
+        ORDER BY STR(?name) STR(?position) ?began ?wdid ?sourceDate`
+}
