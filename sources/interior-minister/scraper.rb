e138a3c3687457ca550d6cdbb51f4fd6dd4a4e34
Tony Bowden <tony@tmtm.com>
2022-05-29 10:51:52 +0300

    compare Interior Ministers

---
+++ sources/interior-minister/scraper.rb
@@ -0,0 +1,57 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+  # Dutch dates
+  class Arabic < WikipediaDate
+    REMAP = {
+      'في المنصب'  => '',
+      'يناير'      => 'January',
+      'فبراير'     => 'February',
+      'مارس'       => 'March',
+      'أبريل'      => 'April',
+      'مايو'       => 'May',
+      'يونيو'      => 'June',
+      'يوليو'      => 'July',
+      'أغسطس'      => 'August',
+      'سبتمبر'     => 'September',
+      'أكتوبر'     => 'October',
+      'نوفمبر'     => 'November',
+      'ديسمبر'     => 'December',
+    }.freeze
+
+    def remap
+      REMAP.merge(super)
+    end
+  end
+
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'الصورة'
+  end
+
+  # No header row in table
+  def holder_entries
+    noko.xpath("//table[.//td[contains(.,'#{header_column}')]][#{table_number}]//tr[td]").drop(1)
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[no name img title dates].freeze
+    end
+
+    def date_class
+      Arabic
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
